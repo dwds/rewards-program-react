ecb7684c000724fccdae8c0a94ada9fb4cf162d8
Derek <derek@dadian-smith.net>
2021-06-02 22:22:10 -0500

    Turn App into functional component

--- src/app.js
+++ src/app.js
@@ -2,10 +2,8 @@ import React from "react";
 import SearchableCustomerTable from "./components/searchableCustomerTable";
 import {customers} from "./data/customerTransactions";
 
-class App extends React.Component {
-  render(){
-    return (<SearchableCustomerTable customers={customers} />);
-  }
+function App() {
+  return <SearchableCustomerTable customers={customers} />;
 }
 
 export default App;
