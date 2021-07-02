809bb8957c487614f654188f8410f64795fca10f
Derek <derek@dadian-smith.net>
2021-07-02 15:17:58 -0500

    Move App to components directory

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,7 @@
 import React from "react";
-import {SearchableCustomerTable} from "./components/searchableCustomerTable";
-import {customers} from "./data/customerTransactions";
+import {SearchableCustomerTable} from "../searchableCustomerTable";
+// import styles from './app.module.css';
+import {customers} from "../../data/customerTransactions";
 
 function App() {
   return <SearchableCustomerTable customers={customers} startDate={new Date(2019, 3)} endDate={new Date(2019, 5)} />;
