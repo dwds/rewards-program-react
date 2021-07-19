c7df9914cd44e9e448d2b0045bd49cae2bb609e3
Derek <derek@dadian-smith.net>
2021-07-19 11:50:12 -0500

    Create InputBase component

---
+++ src/components/inputBase/inputBase.js
@@ -0,0 +1,37 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import styles from './inputBase.module.css';
+
+function InputBase({
+  id,
+  label,
+  onChange = null,
+  placeholder = null,
+  type = "text",
+  value = "",
+  ...other
+}) {
+  return (
+    <div className={styles.root}>
+      <label htmlFor={id}>{label}</label>
+      <input
+        id={id}
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        {...other} />
+    </div>
+  );
+}
+
+InputBase.propTypes = {
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  onChange: PropTypes.func,
+  placeholder: PropTypes.string,
+  type: PropTypes.oneOf(["date", "datetime-local", "email", "file", "hidden", "month", "number", "password", "search", "tel", "text", "time", "url", "week"]),
+  value: PropTypes.string
+};
+
+export default InputBase;
