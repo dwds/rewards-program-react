5b17c31ca3ad94ea1ca65c540b149f563ad16dd3
Derek <derek@dadian-smith.net>
2021-08-05 15:49:18 -0500

    Add page title and description

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,6 +6,9 @@ import {customers} from "../../data/customerTransactions";
 function App() {
   return (
     <div className={styles.root}>
+      <h1>Searchable Point Calculation Table in React</h1>
+      <p>This is a searchable table of customer data that takes customer transaction data and calculates the points earned from purchases within a selectable range of months.</p>
+      <p>See the source code on <a href="https://github.com/dwds/rewards-program-react">GitHub</a>.</p>
       <SearchableCustomerTable customers={customers} startDate={new Date(2019, 3)} endDate={new Date(2019, 5)} />
     </div>
   )
